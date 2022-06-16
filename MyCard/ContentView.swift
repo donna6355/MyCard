2177aa2ec66d283ea373e8b2f67b76d30f8b173b
Donna <donnajun1214@gmail.com>
2022-06-16 10:00:11 +0900

    draft ui layout

--- MyCard/ContentView.swift
+++ MyCard/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        MyCardView()
+        ZStack{
+            Image("GreenBackground")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("DiceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n:1)
+                    DiceView(n:1)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                
+            }
+        }
         
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("Dice\(n)")
+            .resizable()
+            .aspectRatio(1,contentMode: .fit)
+            .padding(.all)
     }
 }
 
